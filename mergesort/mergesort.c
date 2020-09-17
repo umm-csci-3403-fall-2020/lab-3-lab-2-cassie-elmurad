05ee5a23e898d3af0a1d5f3695dea6666d7060a1
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-17 11:55:28 -0500

    I have gotten a start on `mergesort` working to translate the given example from java to C. I have all of the parts there, but I believe some order needs to be changed around.  not yet compiling

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,7 +1,58 @@
 #include "mergesort.h"
+#include<stdlib.h>
 
-void mergesort(int, int*) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
-  return;
+void mergeRanges(int values[], int size, int startIndex, int midPoint, int endIndex){
+  int rangeSize = endIndex - startIndex;
+  int* destination;
+  destination = (int*) calloc(size+1, sizeof(int));
+  int firstIndex = startIndex;
+  int secondIndex = midPoint;
+  int copyIndex = 0;
+  while(firstIndex < midPoint && secondIndex < endIndex){
+    if(values[firstIndex] < values[secondIndex]){
+      destination[copyIndex] = values[firstIndex];
+      firstIndex++;		   
+    }else {
+      destination[copyIndex] = values[secondIndex];  
+      secondIndex++;
+    }  
+    copyIndex++;
+  }
+  while(firstIndex < midPoint) {
+    destination[copyIndex] = values[firstIndex];
+    copyIndex++;
+    firstIndex++;
+  }
+  while(secondIndex < endIndex) {
+    destination[copyIndex] = values[secondIndex];
+    copyIndex++;
+    secondIndex++;
+  }
+  for(int i =0; i< rangeSize; i++){
+    values[i + startIndex] = destination[i];
+  }
+}
+
+void bool needsSorting(int rangeSize){
+  if(rangeSize >= 2){
+    return True; 
+  }else {
+    return False;	  
+  } 
+   
+}
+
+
+void mergeSortRange(int values[], int startIndex, int endIndex){                                                    int rangeSize = endIndex - startIndex;
+    if (needsSorting(rangeSize)) {
+      int midPoint = (startIndex + endIndex) / 2;
+      mergeSortRange(values, startIndex, midPoint);
+      mergeSortRange(values,midPoint, endIndex);
+      mergeRanges(values, startIndex, midPoint, endIndex);
+    }
+}
+
+
+void mergesort(int size , int values[]) {
+  mergeSortRange(values, 0, size); 
 }
