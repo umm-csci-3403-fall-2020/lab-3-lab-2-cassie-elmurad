10e5c0cdc02a6f8359fa9dcdfaa4cbbd318765a0
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-19 14:54:41 -0500

    I added mergesort and all the associated files over to the `array_merge` dir so that sorting could be used within the method. I have also gotten a start on the `array_merge` method, but as of now it is getting a seg fault. and that needs to be addressed obviously. I made helper functions that concationate all the value arrays into on big array, sort the big array, and another that removes repeated values.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,62 @@
 #include "array_merge.h"
+#include "mergesort.h"
+#include "mergesort.c"
+#include<stdlib.h>
+#include<cstdio>
+int countUnique(int* arr, int size){
+  int index =0;
+  int count = 0; 
+  // While number exist continue to check for repeat values
+  while( index < size-1 ){
+    int curr = arr[index];
+    int next = arr[index+1]; 
+    if(curr == next ){
+       index++;
+    }
+    else{
+    count++;
+    index++;
+    }
+  }
+  return count;
+}// end count Unique
+int* catArray(int num_arrays, int* sizes, int** values){
+  int length = 0;
+  for(int i=0; i < num_arrays; i++){
+	length += sizes[i];
+  } 
+  int* catArr = (int*) calloc(length +1, sizeof(int)); 
+  int index = 1;
+  catArr[0] = length+1;
+  while(num_arrays >= 0){
+     int arrSize = sizes[num_arrays];
+     for(int i =0; i< arrSize-1; i++){
+       catArr[index]= values[num_arrays][i];
+       index++;
+     }
+     num_arrays--;  
+  }
+ return catArr;
+}// end cat method
 
-int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
-  return sizes;
+int* removeRepeats(int* arr, int size, int resultSize){
+  int* result = (int*) calloc(resultSize+1, sizeof(int));
+  mergesort(size, arr);
+  int index =0;
+  for(int i=1; i< size-1; i++){
+    int curr = arr[i]; 
+    int next = arr[i+1];
+    if(curr!=next){
+     result[index] = arr[i];
+     index++;
+    }
+  }
+  return result;
 }
+int* array_merge(int num_arrays, int* sizes, int** values) {
+  int* catArr = catArray(num_arrays, sizes, values);
+  int size = catArr[0]; 
+  int unique = countUnique(catArr, size);
+  int* result = removeRepeats(catArr, size, unique);
+     return result;
+ }
---
+++ array_merge/mergesort.c
@@ -0,0 +1,61 @@
+#include "mergesort.h"
+#include<stdlib.h>
+
+void mergeRanges(int values[], int size, int startIndex, int midPoint, int endIndex){
+  int rangeSize = endIndex - startIndex;
+  int* destination;
+  destination = (int*) calloc(size+1, sizeof(int));
+  int firstIndex = startIndex;
+  int secondIndex = midPoint;
+  int copyIndex = 0;
+  while(firstIndex < midPoint && secondIndex < endIndex){
+    if(values[firstIndex] < values[secondIndex]){
+      destination[copyIndex] = values[firstIndex];
+      firstIndex++;		   
+    }else {
+      destination[copyIndex] = values[secondIndex];  
+      secondIndex++;
+    }  
+    copyIndex++;
+  }
+  while(firstIndex < midPoint) {
+    destination[copyIndex] = values[firstIndex];
+    copyIndex++;
+    firstIndex++;
+  }
+  while(secondIndex < endIndex) {
+    destination[copyIndex] = values[secondIndex];
+    copyIndex++;
+    secondIndex++;
+  }
+  for(int i =0; i< rangeSize; i++){
+    values[i + startIndex] = destination[i];
+  }
+  free(destination);
+}
+
+bool needsSorting(int rangeSize){
+ if(rangeSize>=2){
+   return true;
+ } 
+ else{
+   return false;
+ }			
+}
+
+
+void mergeSortRange(int values[], int startIndex, int endIndex){                                                                                                                    int rangeSize = endIndex - startIndex;
+    bool needSort = needsSorting(rangeSize); 
+    if (needSort) {
+      int midPoint = (startIndex + endIndex) / 2;
+      mergeSortRange(values, startIndex, midPoint);
+      mergeSortRange(values,midPoint, endIndex);
+      mergeRanges(values, rangeSize, startIndex, midPoint, endIndex);
+    }
+ 
+}
+
+
+void mergesort(int size , int values[]) {
+  mergeSortRange(values, 0, size); 
+}
